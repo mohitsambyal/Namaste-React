3551e93e3d4e678eaff69daa669343e30a2cb1be
msambyal <msambyal@texala.net>
2022-12-31 22:55:57 +0530

    my first commit

---
+++ Chapter-02/app.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Namaste React"
+);
+const heading2 = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Namaste React Again"
+);
+const heading3 = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Namaste Everyone!"
+);
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading, heading2, heading3]
+);
+console.log(container);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
